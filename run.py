8a0e6791756caa92fb108ab5d0a0a6fb69286558
spaun <you@example.com>
2016-01-31 18:00:55 +0200

    m

--- run.py
+++ run.py
@@ -5,18 +5,19 @@ import time
 driver = SeleniumWebDriver()
 
 
-def run_app():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(driver.run, 'interval', seconds=10, id=str(time.time()))
-    scheduler.start()
-    driver.run()
-    try:
-        # This is here to simulate application activity (which keeps the main thread alive).
-        while True:
-            time.sleep(2)
-    except (KeyboardInterrupt, SystemExit):
-        # Not strictly necessary if daemonic mode is enabled but should be done if possible
-        scheduler.shutdown()
-
+class RunApp:
+    @staticmethod
+    def run():
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(driver.run, 'interval', seconds=10, id=str(time.time()))
+        scheduler.start()
+        driver.run()
+        try:
+            # This is here to simulate application activity (which keeps the main thread alive).
+            while True:
+                time.sleep(2)
+        except (KeyboardInterrupt, SystemExit):
+            # Not strictly necessary if daemonic mode is enabled but should be done if possible
+            scheduler.shutdown()
 if __name__ == '__main__':
-    run_app()
+    RunApp.run()
