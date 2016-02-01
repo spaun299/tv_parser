abeb77143c89114a19f412165545b86acf8ebd3b
spaun299 <spaun1002@gmail.com>
2016-02-01 12:01:24 +0200

    driver capabilities

--- parser_app/parser.py
+++ parser_app/parser.py
@@ -7,7 +7,7 @@ import os
 
 class SeleniumWebDriver(object):
 
-    def __init__(self, url='http://www.work.ua'):
+    def __init__(self, url=config.MAIN_PARSE_URL):
         self.driver = self.get_phantomjs_driver()
         self.url = url
 
@@ -18,7 +18,9 @@ class SeleniumWebDriver(object):
             # conf['service_log_path'] = os.environ.get('OPENSHIFT_PYTHON_LOG_DIR')+'/ghostdriver.log'
             # conf['executable_path'] = os.environ.get('OPENSHIFT_DATA_DIR') + '/phantomjs/bin/phantomjs'
             # conf['service_args'].append('--webdriver={ip}:15002'.format(ip=os.environ.get('OPENSHIFT_PYTHON_IP')))
-            driver = webdriver.Remote(command_executor=os.environ.get('OPENSHIFT_PYTHON_IP')+'/15002')
+            capabilities = dict(browserName='phantomjs', acceptSslCerts=True, javascriptEnabled=True)
+            driver = webdriver.Remote(command_executor=os.environ.get('OPENSHIFT_PYTHON_IP')+'/15002',
+                                      desired_capabilities=capabilities)
             return driver
         driver = webdriver.PhantomJS(**conf)
         return driver
@@ -26,18 +28,15 @@ class SeleniumWebDriver(object):
     def run(self):
         self.driver.get(self.url)
         self.driver.set_window_size(1920, 1080)
-        print('/////////////////////')
-        print(self.driver.current_url)
-        print('/////////////////////')
-        # page_height = 0
-        # scroll_height_script = """ return window.innerHeight + window.scrollY """
+        page_height = 0
+        scroll_height_script = """ return window.innerHeight + window.scrollY """
 
-        # while page_height != self.driver.execute_script(scroll_height_script):
-        #     page_height = self.driver.execute_script(scroll_height_script)
-        #     self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        #     for a in self.driver.find_elements_by_class_name('tv-channel-title__link'):
-        #         print(a.text)
-        #     time.sleep(5)
-        # channels_title_div = self.driver.find_elements_by_class_name('tv-channel-title__link')
-        # channels_url = map(lambda div: div.get_attribute('href'), filter(lambda href: href.get_attribute('href'),
-        #                    channels_title_div))
+        while page_height != self.driver.execute_script(scroll_height_script):
+            page_height = self.driver.execute_script(scroll_height_script)
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            for a in self.driver.find_elements_by_class_name('tv-channel-title__link'):
+                print(a.text)
+            time.sleep(5)
+        channels_title_div = self.driver.find_elements_by_class_name('tv-channel-title__link')
+        channels_url = map(lambda div: div.get_attribute('href'), filter(lambda href: href.get_attribute('href'),
+                           channels_title_div))
