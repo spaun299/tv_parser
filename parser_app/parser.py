095a7837afa2f60c5bd55092a5b15addafc9db99
spaun299 <spaun1002@gmail.com>
2016-02-01 11:45:59 +0200

    m

--- parser_app/parser.py
+++ parser_app/parser.py
@@ -26,15 +26,18 @@ class SeleniumWebDriver(object):
     def run(self):
         self.driver.get(self.url)
         self.driver.set_window_size(1920, 1080)
-        page_height = 0
-        scroll_height_script = """ return window.innerHeight + window.scrollY """
+        print('/////////////////////')
+        print(self.driver.current_url)
+        print('/////////////////////')
+        # page_height = 0
+        # scroll_height_script = """ return window.innerHeight + window.scrollY """
 
-        while page_height != self.driver.execute_script(scroll_height_script):
-            page_height = self.driver.execute_script(scroll_height_script)
-            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            for a in self.driver.find_elements_by_class_name('tv-channel-title__link'):
-                print(a.text)
-            time.sleep(5)
-        channels_title_div = self.driver.find_elements_by_class_name('tv-channel-title__link')
-        channels_url = map(lambda div: div.get_attribute('href'), filter(lambda href: href.get_attribute('href'),
-                           channels_title_div))
+        # while page_height != self.driver.execute_script(scroll_height_script):
+        #     page_height = self.driver.execute_script(scroll_height_script)
+        #     self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        #     for a in self.driver.find_elements_by_class_name('tv-channel-title__link'):
+        #         print(a.text)
+        #     time.sleep(5)
+        # channels_title_div = self.driver.find_elements_by_class_name('tv-channel-title__link')
+        # channels_url = map(lambda div: div.get_attribute('href'), filter(lambda href: href.get_attribute('href'),
+        #                    channels_title_div))
