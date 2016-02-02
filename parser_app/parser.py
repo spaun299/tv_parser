ba9a748e5c8d0b5fb0a9017e0dc006eea88a579d
spaun299 <spaun1002@gmail.com>
2016-02-02 12:09:41 +0200

    comment scheduler

--- parser_app/parser.py
+++ parser_app/parser.py
@@ -36,9 +36,9 @@ class SeleniumWebDriver(object):
         while page_height != self.driver.execute_script(scroll_height_script):
             page_height = self.driver.execute_script(scroll_height_script)
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            for a in self.driver.find_elements_by_class_name('tv-channel-title__link'):
+                print(a.get_attribute('href'))
             time.sleep(5)
         channels_title_div = self.driver.find_elements_by_class_name('tv-channel-title__link')
-        for a in channels_title_div:
-            print(a.get_attribute('href'))
         channels_url = map(lambda div: div.get_attribute('href'), filter(lambda href: href.get_attribute('href'),
                            channels_title_div))
--- run.py
+++ run.py
@@ -6,17 +6,18 @@ driver = SeleniumWebDriver()
 
 
 def run(*args):
-    print(args)
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(driver.run, 'interval', seconds=10, id=str(time.time()))
-    scheduler.start()
     driver.run()
-    try:
-            # This is here to simulate application activity (which keeps the main thread alive).
-        while True:
-            time.sleep(2)
-    except (KeyboardInterrupt, SystemExit):
-            # Not strictly necessary if daemonic mode is enabled but should be done if possible
-        scheduler.shutdown()
+    # print(args)
+    # scheduler = BackgroundScheduler()
+    # scheduler.add_job(driver.run, 'interval', seconds=10, id=str(time.time()))
+    # scheduler.start()
+    # driver.run()
+    # try:
+    #         This is here to simulate application activity (which keeps the main thread alive).
+        # while True:
+        #     time.sleep(2)
+    # except (KeyboardInterrupt, SystemExit):
+    #         Not strictly necessary if daemonic mode is enabled but should be done if possible
+        # scheduler.shutdown()
 if __name__ == '__main__':
     run()
