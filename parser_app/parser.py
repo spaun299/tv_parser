3a7d0cf6f090007c6581de767cf9d2af8b1e39a4
spaun299 <spaun1002@gmail.com>
2016-02-02 13:54:10 +0200

    get urls

--- parser_app/parser.py
+++ parser_app/parser.py
@@ -14,6 +14,16 @@ class SeleniumWebDriver(object):
         self.url = url
 
     @staticmethod
+    def get_channel_xpath():
+        return "//div/div[@class='tv-grid__items']/div[@class='tv-grid__page']/div" \
+               "[@class='tv-grid__item tv-grid__item_is-now_no']/div[@class='tv-channel']/" \
+               "div[@class='tv-channel__title']/div/div[@class='tv-channel-title__link']/a"
+
+    @staticmethod
+    def get_channel_css_selector():
+        return 'div.tv-channel__title > div[class$="tv-channel-title_js_inited"] > div.tv-channel-title__link > a'
+
+    @staticmethod
     def get_phantomjs_driver():
         conf = dict(service_args=['--ssl-protocol=any'])
         if os.environ.get('OPENSHIFT_DATA_DIR'):
@@ -31,14 +41,22 @@ class SeleniumWebDriver(object):
         self.driver.get(self.url)
         self.driver.set_window_size(1920, 1080)
         page_height = 0
+        elements = {}
         scroll_height_script = """ return window.innerHeight + window.scrollY """
-
         while page_height != self.driver.execute_script(scroll_height_script):
             page_height = self.driver.execute_script(scroll_height_script)
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            for a in self.driver.find_elements_by_class_name('tv-channel-title__link'):
-                print(a.get_attribute('href'))
-            time.sleep(5)
+            for a in self.driver.find_elements_by_css_selector(self.get_channel_css_selector()):
+                name = a.find_element_by_css_selector('span.tv-channel-title__text').text
+                href = a.get_attribute('href')
+                icon = a.find_element_by_css_selector('div.tv-channel-title__icon > '
+                                                      'span[class$="image_type_channel"] > span')
+                if (a.get_attribute('href') is not None) and (name not in elements.keys()):
+                    elements[name] = {'href': href, 'icon': icon}
+
+
+                print(elements)
+            time.sleep(2)
         channels_title_div = self.driver.find_elements_by_class_name('tv-channel-title__link')
         channels_url = map(lambda div: div.get_attribute('href'), filter(lambda href: href.get_attribute('href'),
                            channels_title_div))
--- readme.md
+++ readme.md
@@ -9,11 +9,16 @@
     ssh to your openshift app
     cd $OPENSHIFT_DATA_DIR/phantomjs/bin
     chmod +x phantomjs
-    if you want change ghostdriver port, you can do it by modify .openshift/action_hooks/post_deploy file.
+    if file '.openshift/action_hooks/post_deploy' exists and you want change ghostdriver port, 
+    you can do it by modify .openshift/action_hooks/post_deploy file.
     But then you have to run 'chmod +x .openshift/action_hooks/post_deploy'
+    if file '.openshift/action_hooks/post_deploy' does't exists you should create this one within:
+    **
+    #!/bin/bash
+    nohup ${OPENSHIFT_DATA_DIR}/phantomjs/bin/phantomjs --webdriver=$OPENSHIFT_PYTHON_IP:15005 &
+    **
     2:)install necessary libraries:
     source venv/bin/activate
     pip instal -r requirements.txt
     3:)install and configure rhc tool:
     https://developers.openshift.com/en/getting-started-debian-ubuntu.html#client-tools
-
