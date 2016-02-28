78527855cc3c26279cd9d1f15e467f1dcd65c4fd
spaun299 <spaun1002@gmail.com>
2016-02-28 16:46:50 +0200

    delete bash cron

--- run_scheduler.py
+++ run_scheduler.py
@@ -10,25 +10,25 @@ driver = SeleniumWebDriver()
 send_email = SendEmail().send_email
 
 
-def run_scheduler(type_of_parser):
+def run_scheduler():
     try:
         write_to_log('Run scheduler')
-        if type_of_parser == 'channels':
-            write_to_log('Preparing to parse channels')
-            driver.parse_url_channels()
-        elif type_of_parser == 'programs':
-            write_to_log('Preparing to parse programs')
-            driver.parse_tv_programs()
-        # schedule.every(3).hours.do(driver.parse_tv_programs)
-        # schedule.every(4).hours.do(driver.parse_url_channels)
+        # if type_of_parser == 'channels':
+        #     write_to_log('Preparing to parse channels')
+        #     driver.parse_url_channels()
+        # elif type_of_parser == 'programs':
+        #     write_to_log('Preparing to parse programs')
+        #     driver.parse_tv_programs()
+        schedule.every().day.at("09:52").do(driver.parse_tv_programs)
+        schedule.every().day.at("10:50").do(driver.parse_url_channels)
         # schedule.every().monday.at('"06:00"').do(driver.parse_tv_programs)
         # schedule.every().sunday.at("'12:00'").do(driver.parse_url_channels)
         # while True:
         #     schedule.run_pending()
         #     time.sleep(2)
-        else:
-            subprocess.Popen("echo Unknown argument for parser."
-                             "Type --help for more information".split())
+        # else:
+        #     subprocess.Popen("echo Unknown argument for parser."
+        #                      "Type --help for more information".split())
     except Exception as e:
         write_to_log(error=e)
         send_email(subject='Parse error. Exception', exception=e)
@@ -41,8 +41,8 @@ def run_scheduler(type_of_parser):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('type', help='Parser type: channels or programs')
-    args = parser.parse_args()
-    type_of_parser = args.type.split("=")[-1]
-    run_scheduler(type_of_parser)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('type', help='Parser type: channels or programs')
+    # args = parser.parse_args()
+    # type_of_parser = args.type.split("=")[-1]
+    run_scheduler()
